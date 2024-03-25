9285874f9ce335bdf0124dc3c9ff8a3878ad042e
myselfjoraj <myselfjoraj@gmail.com>
2024-03-25 12:41:55 +0530

    asus commit

--- database.py
+++ database.py
@@ -26,6 +26,22 @@ class Database:
         data = cur.fetchall()
         cur.close()
         modal_instances = [FileModel.from_tuple(row) for row in data]
+
+    def getUsersTableCreationStatement():
+        return '''create table users(username varchar(22) 
+        primary key,password varchar(22),email varchar(22),
+        first_name varchar(22),last_name varchar(22),city varchar(22),
+        gender varchar(22),dob varchar(22),martial_status varchar(22),
+        age int,number varchar(22),url varchar(500),
+        used_storage varchar(500));'''
+    
+    def getFileDetailsTableCreationStatement():
+        return '''create table file_details(id int primary key auto_increment,
+        username varchar(22),file_name varchar(22),timestamp varchar(22),
+        file_count varchar(22),uri varchar(500),modified varchar(22),
+        size varchar(22),extension varchar(22),mode varchar(22),
+        type varchar(22));'''
+        
         
 
     
