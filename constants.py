ca2c596681c8f9f3fd3d8b7200290dabc85c9f76
Joraj J R <myselfjoraj@gmail.com>
2024-05-02 10:17:49 +0530

    Mac Os Commit

--- constants.py
+++ constants.py
@@ -1,16 +1,22 @@
-
-
 def getSecretKey():
     return "vpJifWLQnZFehKw5PtOrJDT5QZDIhwqg"
+
 
 def getSqlHost():
     return "localhost"
 
+
 def getSqlUser():
     return "root"
+
 
 def getSqlPassword():
     return "Joraj@01"
 
+
 def getSqlMainDatabase():
     return "CodeNestDB"
+
+
+def url_to_route_cors():
+    return 'https://cors-anywhere.herokuapp.com/http://api.brainshop.ai/get?bid=181681&key=cpsSyYyIGcb8jAny&uid=abhijith&msg=haai'
